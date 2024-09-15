70318af44cec7e325f7c85fb274556a75c70eb3f
Myan Gupta <ayangupta1@infra04-wg013.eduroam.wireless.umn.edu>
2024-09-15 10:27:55 -0500

    first commit

---
+++ api/databases/migrations/2024_09_14_224149_user.py
@@ -0,0 +1,30 @@
+"""User Migration."""
+
+from masoniteorm.migrations import Migration
+
+
+class User(Migration):
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.create("users") as table:
+            table.increments("id")
+            table.string("first_name")
+            table.string("middle_name")
+            table.string("surname")
+            table.string("email").unique()
+            table.string("cellphone")
+            table.string("password")
+            table.enum("gender" , ["m" , "f"])
+            table.string("city")
+            table.string("state")
+            table.string("zipcode")
+            table.string("timezone")
+            table.timestamps()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop("users")
